64bb7de81a39e6f8a9c9bd550b0517ac8e435769
Serhiy Mazur <pomet228@gmail.com>
2017-09-09 21:28:49 +0300

    almost first step

--- src/Logic.java
+++ src/Logic.java
@@ -5,31 +5,29 @@ import java.util.Scanner;
  * Created by IIDEMONII on 08.09.2017.
  */
 public class Logic {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int cowsnum,bullnum,guess;
-        int number;
-        boolean yes;
-        Random generator;
-        int[] array = new int[4];
-        generator = new Random();
-        for(int i=0; i<4; i++)
-        {
-            number = generator.nextInt( 9 );
-            yes=true;
-            for(int j=0; j<=i; j++)
-            {
-                if(number==array[j])
-                    yes=false;
+    Logic(){
+            Random random = new Random();
+            int rage = 9999;
+            int  generator = 1000 + random.nextInt(rage - 1000);
+        System.out.println(Integer.toString(generator));
+        }
 
-            }
-            if(yes){
-                array[i]=number;
-                System.out.print(number);
-            }else{
-                i--;
-            }
+    public static void main(String[] args) {
+        Logic Log = new Logic();
+        boolean yes;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int guess = scan.nextInt();
+        Integer.toString(guess);
+        System.out.println(guess.equals(generator));
+
+
+
+
+
 
     }
 
-}}
+
+
+    }
