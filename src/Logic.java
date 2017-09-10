3eb2d5cbd1d000e4b5eefe8ad8e18da7228e8e95
Serhiy Mazur <pomet228@gmail.com>
2017-09-10 10:52:19 +0300

    new try

--- src/Logic.java
+++ src/Logic.java
@@ -5,23 +5,41 @@ import java.util.Scanner;
  * Created by IIDEMONII on 08.09.2017.
  */
 public class Logic {
-    Logic(){
-            Random random = new Random();
-            int rage = 9999;
-            int  generator = 1000 + random.nextInt(rage - 1000);
-        System.out.println(Integer.toString(generator));
-        }
+
 
     public static void main(String[] args) {
-        Logic Log = new Logic();
-        boolean yes;
+
+        String guess  ;
+
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number:");
-        int guess = scan.nextInt();
-        Integer.toString(guess);
-        System.out.println(guess.equals(generator));
+        int number;
+        boolean yes;
+        Random generator;
+        int[] array = new int[4];
+        generator = new Random();
+        for(int i=0; i<4; i++)
+        {
+            number = generator.nextInt( 10 );
+            yes=true;
+            for(int j=0; j<=i; j++)
+            {
+                if(number==array[j])
+                    yes=false;
 
+            }
+            if(yes){
+                array[i]=number;
+                System.out.print(number);
+            }else{
+                i--;
+            }
 
+        }
+        guess = scan.next();
+        char[] chararay = guess.toCharArray();
+        for(int i = 0;i<4;i++){
+            System.out.print(chararay[i]);
+        }
 
 
 
