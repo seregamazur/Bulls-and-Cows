668d6c3ae8d4c1f4b0e666bad3e13c2d3c28b932
Serhiy Mazur <pomet228@gmail.com>
2017-09-11 19:47:39 +0300

    console version without advanced structure

--- src/Logic.java
+++ src/Logic.java
@@ -1,62 +1,53 @@
-import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Created by IIDEMONII on 08.09.2017.
- */
-public class Logic {
-    public static void main(String[] args) {
-        int number;
-        String guess  ;
-        Scanner scan = new Scanner(System.in);
-        boolean yes;
-        Random generator;
-        int[] array = new int[4];
-        generator = new Random();
-        for(int i=0; i<4; i++)
-        {
-            number = generator.nextInt( 10 );
-            yes=true;
-            for(int j=0; j<=i; j++)
-            {
-                if(number==array[j])
-                    yes=false;
-
+public class Logic{
+    public static void main(String[] args){
+        Random gen = new Random();
+        int numb = 0;
+        while(hasDupes(numb = (gen.nextInt(9000) + 1000)));
+        System.out.println(numb);
+        String numbStr = numb +"";
+        boolean guessed = false;
+        Scanner input = new Scanner(System.in);
+        int guesses = 0;
+        do{
+            int bullcount = 0;
+            int cowcount = 0;
+            System.out.print("Введіть ваше 4-х значне число:");
+            int guess;
+            try{
+                guess = input.nextInt();
+                if(hasDupes(guess) || guess < 1000) continue;
+            }catch(InputMismatchException e){
+                continue;
             }
-            if(yes){
-                array[i]=number;
-                System.out.print(number);
+            guesses++;
+            String guessStr = guess + "";
+            for(int i= 0;i < 4;i++){
+                if(guessStr.charAt(i) == numbStr.charAt(i)){
+                    bullcount++;
+                }else if(numbStr.contains(guessStr.charAt(i)+"")){
+                    cowcount++;
+                }
+            }
+            if(bullcount == 4){
+                guessed = true;
             }else{
-                i--;
+                System.out.println(cowcount+" Корів і "+bullcount+" биків");
             }
-
-        }
-        guess = scan.next();
-        String strArr[] = guess.split("");
-        int numArr[] = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++) {
-            numArr[i] = Integer.parseInt(strArr[i]);
-        }
-        int Bullcount = 0, cowcount = 0;
-        for(int i = 0;i<numArr.length;i++) {
-            if (numArr[i] == array[i]) { Bullcount++ ; --cowcount;}
-        }
-        for(int i = 0;i<numArr.length;i++){
-            for(int j = 0;j<numArr.length;j++){
-                if (numArr[i] == array[i]) cowcount++;
-            }
-        }
-
-
-        System.out.print("Кількість биків:" + Bullcount + "Кількість корів:" + cowcount );
-
-        }
-
-
-
+        }while(!guessed);
+        System.out.println("Ви виграли за "+guesses+" спроб.");
     }
 
-
-
-
+    public static boolean hasDupes(int num){
+        boolean[] digs = new boolean[10];
+        while(num > 0){
+            if(digs[num%10]) return true;
+            digs[num%10] = true;
+            num/= 10;
+        }
+        return false;
+    }
+}
