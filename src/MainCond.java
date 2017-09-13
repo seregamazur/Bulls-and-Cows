4024d488291e8cd3a8bf94b7a70a232b5993e51e
Serhiy Mazur <pomet228@gmail.com>
2017-09-14 00:00:25 +0300

    Console project

---
+++ src/MainCond.java
@@ -0,0 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class MainCond {
+    public void cond(String numbStr){
+    boolean guessed = false;
+    Scanner input = new Scanner(System.in);
+    int guesses = 0;
+        do{
+        int bullcount = 0;
+        int cowcount = 0;
+        System.out.print("Введіть ваше 4-х значне число:");
+        int guess;
+        try{
+            guess = input.nextInt();
+            if(Generate_numb.hasDupes(guess) || guess < 1000) continue;
+        }catch(InputMismatchException e){
+            continue;
+        }
+        guesses++;
+        String guessStr = guess + "";
+        for(int i= 0;i < 4;i++){
+            if(guessStr.charAt(i) == numbStr.charAt(i)){
+                bullcount++;
+            }else if(numbStr.contains(guessStr.charAt(i)+"")){
+                cowcount++;
+            }
+        }
+        if(bullcount == 4){
+            guessed = true;
+        }else{
+            System.out.println(cowcount+" Корів і "+bullcount+" биків");
+        }
+    }while(!guessed);
+        System.out.println("Ви виграли за "+guesses+" спроб.");
+}
+
+}
---
+++ src/game_start.java
@@ -0,0 +1,8 @@
+public class game_start{
+    public static void main(String[] args){
+        Generate_numb start = new Generate_numb();
+        start.go();
+    }
+
+
+}
