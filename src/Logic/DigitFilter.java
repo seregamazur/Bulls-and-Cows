d65b25b81036a44477e83bfdc933798a1be39737
Serhiy Mazur <pomet228@gmail.com>
2017-10-02 21:00:12 +0300

    s

--- src/Logic/DigitFilter.java
+++ src/Logic/DigitFilter.java
@@ -6,7 +6,6 @@ import javax.swing.text.DocumentFilter;
 public class DigitFilter extends DocumentFilter {
     private static final String DIGITS = "\\d+";
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-
         if (string.matches(DIGITS)) {
             super.insertString(fb, offset, string, attr);
         }
