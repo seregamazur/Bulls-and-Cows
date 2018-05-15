eb12800c7ad5353a3b52bf2e2878da8dc0b0017a
seregamazur <serhiy.mazur0@gmail.com>
2018-05-16 00:08:36 +0300

    Java FX #12 View&Controller tests

--- src/test/controller/AgainstCompTest.java
+++ src/test/controller/AgainstCompTest.java
@@ -10,28 +10,22 @@ import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 
 public class AgainstCompTest extends ApplicationTest {
     @Override
-    public void start(Stage stage)  {
-        try{
+    public void start(Stage stage) throws Exception {
         URL url = new File("src/resources/fxml/AgainstComp.fxml").toURI().toURL();
         Parent mainNode = FXMLLoader.load(url);
         stage.setScene(new Scene(mainNode));
         stage.show();
         stage.toFront();
-    } catch (IOException e) {
-        java.util.logging.Logger.getLogger(AgainstCompTest.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
     }
-
-}
     @After
     public void tearDown () throws Exception {
         FxToolkit.hideStage();
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testTextFieldInput () {
        clickOn("#textfield");
         write("1234");
@@ -51,26 +45,26 @@ public class AgainstCompTest extends ApplicationTest {
     public void testBackClick () {
         clickOn("#backbutton");
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testEmptyFieldInput () {
         clickOn("#inputbutton");
 
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testLargeNumberFieldInput ()  {
         clickOn("#textfield");
             write("123456789");
        clickOn("#inputbutton");
     }
-    @Test(expected = NumberFormatException.class)
-    public void testStringFieldInput ()  {
+    @Test
+    public void testStringFieldInput () {
         clickOn("#textfield");
             write("Hello");
         clickOn("#inputbutton");
 
 
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testSameNumbFieldInput () {
         clickOn("#textfield");
             write("1234");
--- src/test/controller/WithCompTest.java
+++ src/test/controller/WithCompTest.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Test;
@@ -10,24 +11,16 @@ import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 
 public class WithCompTest extends ApplicationTest {
     @Override
-    public void start(Stage stage) {
-        try {
-
-
+    public void start(Stage stage) throws Exception {
         URL url = new File("src/resources/fxml/WithComp.fxml").toURI().toURL();
         Parent mainNode = FXMLLoader.load(url);
         stage.setScene(new Scene(mainNode));
         stage.show();
         stage.toFront();
-        } catch (IOException e) {
-            java.util.logging.Logger.getLogger(WithCompTest.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
-        }
-
     }
     @After
     public void tearDown () throws Exception {
@@ -38,20 +31,20 @@ public class WithCompTest extends ApplicationTest {
         clickOn("#backbutton");
 
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testTextFieldInput () {
             clickOn("#textfield");
             write("1234");
         clickOn("#startbutton");
 
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testLargeNumberFieldInput () {
        clickOn("#textfield");
             write("123456789");
         clickOn("#startbutton");
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testEmptyFieldInput () {
         clickOn("#startbutton");
         }
@@ -60,7 +53,7 @@ public class WithCompTest extends ApplicationTest {
         clickOn("#newgamebutton");
 
     }
-    @Test(expected = NumberFormatException.class)
+    @Test
     public void testStringFieldInput () {
         clickOn("#textfield");
             write("Hello");
